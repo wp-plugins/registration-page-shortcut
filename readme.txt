5b909db29cb844334fa126ee6f83a31442756e70
phplaw <phplaw@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-19 09:49:47 +0000

    Add plugin source and Its information to wordpress SVN server
    git-svn-id: https://plugins.svn.wordpress.org/registration-page-shortcut/trunk@1163213 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,33 @@
+=== Registration Page Shortcut ===
+Contributors: phplaw
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=phplaw%40gmail%2ecom&lc=VN&item_name=RegistrationPageShortcut&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donate_SM%2egif%3aNonHosted
+Tags: registration, shortcut, page
+Requires at least: 3.0.1
+Tested up to: 4.2.2
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Plugin allow you to create a page that will display registration form for your user registers.
+
+== Description ==
+
+This plguin allow you to show a registration form for a specific page,
+It is really simple just add [registration_form] shortcut to content of a page.
+If you have any question, suggest features for this plugin,
+Please let me know by send me an email: phplaw@gmail.com
+or you can send message to me via Skype: conan_tpn
+
+== Installation ==
+How to install and config for this plugin.
+1. Upload `registration-page-shortcut.php` to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+2. Create a page that you want to display registration form.
+3. Add shotcut [registration_form] to this page content
+4. Add menu for this page, take a look and enjoy your registration page.
+e.g.
+
+== Changelog ==
+
+= 1.0 =
+* Add plugin to wordpress svn on 20150519
---
+++ registration_page_shortcut.php
@@ -0,0 +1,114 @@
+<?php
+/*
+  Plugin Name: Registration Page Shortcut
+  Plugin URI: https://github.com/phplaw/wordpress-tips-tricks
+  Description: Show registration form on a page, just add [registration_form] to the page to show the form.
+  Version: 1.0
+  Author: Sonny Nguyen
+  Author URI: https://github.com/phplaw
+ */
+
+add_shortcode('registration_form', 'registration_form_handle');
+
+
+function registration_form_handle() {
+  global $errors;
+  $registered = false;
+  ini_set('display_errors', 1);
+  error_reporting(E_ALL);
+
+  $http_post = ('POST' == $_SERVER['REQUEST_METHOD']);
+  $user_login = '';
+  $user_email = '';
+  if ( $http_post ) {
+    $user_login = $_POST['user_login'];
+    $user_email = $_POST['user_email'];
+    $errors = register_new_user($user_login, $user_email);
+    if ( !is_wp_error($errors) ) {
+      /*$redirect_to = !empty( $_POST['redirect_to'] ) ? $_POST['redirect_to'] : 'wp-login.php?checkemail=registered';
+      wp_safe_redirect( $redirect_to );
+      exit();*/
+      $registered = true;
+    }
+  }
+  ob_start();
+  // if is not registered
+  if ($registered === false) :
+    /*action="<?php echo esc_url( site_url('wp-login.php?action=register', 'login_post') ); ?>"*/
+    echo get_registration_page_error($errors);
+    ?>
+    <form name="registerform" id="registerform" method="post" novalidate="novalidate">
+      <p>
+        <label for="user_login"><?php _e('Username') ?><br />
+          <input type="text" name="user_login" id="user_login" class="input" value="<?php echo esc_attr(wp_unslash($user_login)); ?>" size="20" /></label>
+      </p>
+      <p>
+        <label for="user_email"><?php _e('E-mail') ?><br />
+          <input type="email" name="user_email" id="user_email" class="input" value="<?php echo esc_attr( wp_unslash( $user_email ) ); ?>" size="25" /></label>
+      </p>
+      <?php
+      /**
+       * Fires following the 'E-mail' field in the user registration form.
+       *
+       * @since 2.1.0
+       */
+      do_action( 'register_form' );
+      ?>
+      <p id="reg_passmail"><?php _e('A password will be e-mailed to you.') ?></p>
+      <br class="clear" />
+      <p class="submit"><input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="<?php esc_attr_e('Register'); ?>" /></p>
+    </form>
+
+    <p id="nav">
+      <a href="<?php echo esc_url( wp_login_url() ); ?>"><?php _e( 'Log in' ); ?></a> |
+      <a href="<?php echo esc_url( wp_lostpassword_url() ); ?>" title="<?php esc_attr_e( 'Password Lost and Found' ) ?>"><?php _e( 'Lost your password?' ); ?></a>
+    </p>
+  <?php
+  else:
+    ?>
+    <div class="registration_completed"><?php _e('Thanks for registration with us, we will check and approve your account soon.') ?></div>
+  <?php
+  endif;
+  // END registered
+  return ob_get_clean();
+}
+
+function get_registration_page_error($wp_error) {
+  $output = '';
+  if ( is_wp_error($wp_error) ) {
+    $errors = '';
+    $messages = '';
+    foreach ( $wp_error->get_error_codes() as $code ) {
+      $severity = $wp_error->get_error_data( $code );
+      foreach ( $wp_error->get_error_messages( $code ) as $error_message ) {
+        if ( 'message' == $severity )
+          $messages .= '	' . $error_message . "<br />\n";
+        else
+          $errors .= '<li>' . $error_message . "</li>";
+      }
+    }
+    if ( ! empty( $errors ) ) {
+      /**
+       * Filter the error messages displayed above the login form.
+       *
+       * @since 2.1.0
+       *
+       * @param string $errors Login error message.
+       */
+      //$output =  '<ul id="login_error">' . apply_filters( 'login_errors', $errors ) . "</ul>";
+      $output .=  '<ul id="login_error" class="woocommerce-error">' . $errors . "</ul>";
+    }
+    if ( ! empty( $messages ) ) {
+      /**
+       * Filter instructional messages displayed above the login form.
+       *
+       * @since 2.5.0
+       *
+       * @param string $messages Login messages.
+       */
+      $output .=  '<p class="message">' . apply_filters( 'login_messages', $messages ) . "</p>\n";
+    }
+  }
+
+  return '<div class="woocommerce">' . $output . '</div>';
+}
